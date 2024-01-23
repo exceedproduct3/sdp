f53292b62a1e7b2c8ff14bef1ff4e7d23bc18bf0
ahsanfa <ahsanfa@exceedgulf.com>
2024-01-23 11:53:58 +0500

    Added new method in IStemexeHost.kt "getTechnadoptProducts"

--- app/src/main/java/com/android/stemexeframework/IStemexeHost.kt
+++ app/src/main/java/com/android/stemexeframework/IStemexeHost.kt
@@ -1,9 +1,12 @@
 package com.android.stemexeframework
 
+import android.content.Context
+
 interface IStemexeHost {
 
     fun getAuthorizationCode(clientId: String, callback: (result: String?) -> Unit)
     fun getAppAppearanceInformation(callback: (result: StemexeAppSettings?) -> Unit)
     fun getDataProviders(providerType: String, callback: (result: ArrayList<StemexeDataProvider>?) -> Unit)
     fun openUrl(url: String, context: HashMap<String, String>, callback: (result: Boolean?) -> Unit)
+    fun getTechnadoptProducts(context: Context, startPageNumber: Int, search:String, language:String, callback: (result: Any?) -> Unit)
 }
--- app/src/main/java/com/android/stemexeframework/IStemexeModule.kt
+++ app/src/main/java/com/android/stemexeframework/IStemexeModule.kt
@@ -9,6 +9,5 @@ interface IStemexeModule {
     fun activate(config: HashMap<String, String>, callback: (fragment:Fragment) -> Unit)
     fun getDataProviders(providerType: String, callback: (result: ArrayList<StemexeDataProvider>?) -> Unit)
     fun openUrl(url: String, context: HashMap<String, String>, callback: (result: Boolean?) -> Unit)
-    fun getTechnadoptProducts(context: Context, startPageNumber: Int, search:String, language:String, callback: (result: Any?) -> Unit)
 
 }
